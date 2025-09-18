807fac8667d56ef5d2677fa5949d03c5f717e531
Hector Ayala <ayalahec99@gmail.com>
2025-09-18 13:26:24 -0300

    base para la pagina de GitHub

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    host: true,
-    port: 5173,
-  },
+  base: '/speedtech-site',
 })
